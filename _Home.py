4d6cb2becc5f209cb1682485a93e9c26f207a9ae
Shazia Parween <115113710+shazia-if@users.noreply.github.com>
2026-08-18 02:35:29 +0530

    Create StoryVerse homepage with navigation
    Initial implementation of StoryVerse homepage with layout and introduction.

---
+++ _Home.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="StoryVerse",
+                   page_icon="📖",
+                   layout="wide")
+
+st.title("StoryVerse")
+st.subheader("Narrative Planning System for Writers and Authors")
+st.write(
+        """
+    Welcome to StoryVerse.
+
+    Create stories, manage characters,
+    organize scenes, and visualize timelines.
+    """
+)
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col2:
+    if st.button(
+        "🚀 Let's Get Started",
+        use_container_width=True
+    ):
+        st.switch_page("pages/1_Stories.py")
